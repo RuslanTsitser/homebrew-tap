50a7a0f5a19a2a12a3a8ff29249fb64396eafbdb
RuslanTsitser <cicer_ruslan@mail.ru>
2026-09-09 17:27:26 -0300

    Update FloatingTerminal to 1.0.3

--- Casks/floating-terminal.rb
+++ Casks/floating-terminal.rb
@@ -1,8 +1,8 @@
 cask "floating-terminal" do
-  version "1.0.2"
-  sha256 "0461fcdbecc3e4e9fe8ba1c46ce16268eed1d5cff8aa1708f205b39462c178c3"
+  version "1.0.3"
+  sha256 "582ee46365e726042aae84c2c811e4c3745a601286e41aae4a52b2913bcf2417"
 
-  url "https://github.com/RuslanTsitser/FloatingTerminal/releases/download/v1.0.2/FloatingTerminal-1.0.2.zip"
+  url "https://github.com/RuslanTsitser/FloatingTerminal/releases/download/v1.0.3/FloatingTerminal-1.0.3.zip"
   name "FloatingTerminal"
   desc "Drop-down terminal activated by a global hotkey"
   homepage "https://github.com/RuslanTsitser/FloatingTerminal"
